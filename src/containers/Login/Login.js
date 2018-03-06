4bab30f68857af686304d3972fda9811961f9192
Lindsey <lindsey@users-MacBook-Pro.local>
2018-03-06 15:13:58 -0800

    firebase config; changed front page to map to login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text,  } from 'react-native';
 
 class Login extends Component {
   render() {
     return (
-      <View>
-        <Text> Login </Text>
-      </View>
+      <div>
+        <p>Login</p>
+      </div>
     );
   }
 }
