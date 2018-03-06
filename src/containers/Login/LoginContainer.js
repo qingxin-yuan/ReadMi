93cc43f6c6138461e5113ef325a478b32c49b5d6
qingxin-yuan <qingxin.yn@gmail.com>
2018-03-06 14:16:01 -0800

    add login container dummie file

---
+++ src/containers/Login/LoginContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text,  } from 'react-native';
+
+class LoginContainer extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Login?  </Text>
+      </View>
+    );
+  }
+}
+
+export default LoginContainer;
