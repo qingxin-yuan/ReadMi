e7900e7c0a12280c969ffb1b002429df4419a155
Lindsey <lindseywoo@gmail.com>
2018-03-06 15:30:39 -0800

    added email box for login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -4,7 +4,17 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <p>Login</p>
+        <h1> Login </h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              className="form-control"
+              ref={email => (this.email = email)}
+              placeholder="Email"
+            />
+          </div>
+        </form>
       </div>
     );
   }
