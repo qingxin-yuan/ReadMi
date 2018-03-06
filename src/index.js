4e4a66b9f3008916916b9f01c82bffd1e2322468
qingxin-yuan <qingxin.yn@gmail.com>
2018-03-06 14:03:12 -0800

    config folder structure.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        REEEEEEEEADMI
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
